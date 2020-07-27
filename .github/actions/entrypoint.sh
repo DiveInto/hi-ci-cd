fc6ae2799a840c550fb43771ded1f5c74e79d71c
Liu Mingmin <diveintotomorrow@gmail.com>
2020-07-27 19:03:22 +0800

    no hyphen in shell var

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -l
 
 echo "Hello $1"
-echo "Hello ${INPUT_WHO-TO-GREET}"
+echo "Hello ${INPUT_WHO_TO_GREET}"
+echo "Hello $INPUT_WHO_TO_GREET"
 env
 time=$(date)
 echo "::set-output name=time::$time"
