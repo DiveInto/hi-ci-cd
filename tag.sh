850b663ff1bf06950d151e97249e26d0e9da2e4c
Liu Mingmin <diveintotomorrow@gmail.com>
2021-03-23 16:13:18 +0800

    clean script

--- tag.sh
+++ tag.sh
@@ -3,16 +3,6 @@
 # Notes: based on https://stackoverflow.com/a/27332476/404145
 #        with small modifications
 
-VERSION=""
-
-#get parameters
-while getopts v: flag
-do
-  case "${flag}" in
-    v) VERSION=${OPTARG};;
-  esac
-done
-
 #get highest tag number, and add default if doesn't exist
 CURRENT_VERSION=`git describe --abbrev=0 --tags 2>&1`
 #echo $CURRENT_VERSION
@@ -33,21 +23,6 @@ VNUM2=`echo ${CURRENT_VERSION_PARTS[1]} | sed 's/[^0-9]*//g'`
 VNUM3=`echo ${CURRENT_VERSION_PARTS[2]} | sed 's/[^0-9]*//g'`
 #echo "nums $VNUM1 $VNUM2 $VNUM3"
 
-#if [[ $VERSION == 'major' ]]
-#then
-#  VNUM1=$((VNUM1+1))
-#elif [[ $VERSION == 'minor' ]]
-#then
-#  VNUM2=$((VNUM2+1))
-#elif [[ $VERSION == 'patch' ]]
-#then
-#  VNUM3=$((VNUM3+1))
-#  #VNUM3=$(printf "%03g" $VNUM3)
-#else
-#  echo "No version type (https://semver.org/) or incorrect type specified, try: -v [major, minor, patch]"
-#  exit 1
-#fi
-
 IS_AUTO_TAG=0
 if [[ $CURRENT_VERSION == *"-pre."*  ]]; then
     IS_AUTO_TAG=1
@@ -61,6 +36,7 @@ NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
 #to publish, need to be logged in to npm, and with clean working directory: `npm login; git stash`
 if [ -z "$NEEDS_TAG" ]; then
   #create new tag
+
   if [ $IS_AUTO_TAG -eq 0 ]; then
       VNUM3=$(expr $VNUM3 + 1)
   fi
@@ -71,7 +47,7 @@ if [ -z "$NEEDS_TAG" ]; then
   git tag $NEW_TAG
   echo "Tagged with $NEW_TAG"
 
-  #git push --tags
+  git push --tags
   #git push
 else
   echo "Already a tag on this commit"
