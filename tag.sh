bede6db38b901af984e6016a43da441a3cf3b525
Liu Mingmin <diveintotomorrow@gmail.com>
2021-03-23 16:06:29 +0800

    update auto tag logic

--- tag.sh
+++ tag.sh
@@ -33,25 +33,25 @@ VNUM2=`echo ${CURRENT_VERSION_PARTS[1]} | sed 's/[^0-9]*//g'`
 VNUM3=`echo ${CURRENT_VERSION_PARTS[2]} | sed 's/[^0-9]*//g'`
 #echo "nums $VNUM1 $VNUM2 $VNUM3"
 
-if [[ $VERSION == 'major' ]]
-then
-  VNUM1=$((VNUM1+1))
-elif [[ $VERSION == 'minor' ]]
-then
-  VNUM2=$((VNUM2+1))
-elif [[ $VERSION == 'patch' ]]
-then
-  VNUM3=$((VNUM3+1))
-  VNUM3=$(printf "%03g" $VNUM3)
-else
-  echo "No version type (https://semver.org/) or incorrect type specified, try: -v [major, minor, patch]"
-  exit 1
+#if [[ $VERSION == 'major' ]]
+#then
+#  VNUM1=$((VNUM1+1))
+#elif [[ $VERSION == 'minor' ]]
+#then
+#  VNUM2=$((VNUM2+1))
+#elif [[ $VERSION == 'patch' ]]
+#then
+#  VNUM3=$((VNUM3+1))
+#  #VNUM3=$(printf "%03g" $VNUM3)
+#else
+#  echo "No version type (https://semver.org/) or incorrect type specified, try: -v [major, minor, patch]"
+#  exit 1
+#fi
+
+IS_AUTO_TAG=0
+if [[ $CURRENT_VERSION == *"-pre."*  ]]; then
+    IS_AUTO_TAG=1
 fi
-
-
-#create new tag
-NEW_TAG="v$VNUM1.$VNUM2.$VNUM3-auto"
-#echo "($VERSION) updating $CURRENT_VERSION to $NEW_TAG."
 
 #get current hash and see if it already has a tag
 GIT_COMMIT=`git rev-parse HEAD`
@@ -60,10 +60,18 @@ NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
 #only tag if no tag already
 #to publish, need to be logged in to npm, and with clean working directory: `npm login; git stash`
 if [ -z "$NEEDS_TAG" ]; then
+  #create new tag
+  if [ $IS_AUTO_TAG -eq 0 ]; then
+    VNUM3=$((VNUM3+1))
+  fi
+
+  NEW_TAG="v$VNUM1.$VNUM2.$VNUM3-pre.$(date +"%Y%m%d%H%M")"
+  #echo "($VERSION) updating $CURRENT_VERSION to $NEW_TAG."
+
   git tag $NEW_TAG
   echo "Tagged with $NEW_TAG"
 
-  git push --tags
+  #git push --tags
   #git push
 else
   echo "Already a tag on this commit"
